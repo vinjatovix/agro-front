ff64e48f389dbbb23e5e93c4673e599f33acf02b
vinjadevix <vinjatovix@gmail.com>
2026-06-05 00:43:43 +0200

    feat(plants): add aliases display and badge to PlantCard component

--- src/pages/plants/PlantCard/PlantCard.tsx
+++ src/pages/plants/PlantCard/PlantCard.tsx
@@ -4,6 +4,8 @@ import { capitalizeFirstLetter } from '../../../shared/utils/capitalizeFirstLett
 import { findPlantImage } from '../utils/findPlantImage';
 
 import './plantCard.css';
+import Badge from '../../../shared/components/Badge/Bagde';
+import { iconsMap } from '../../../shared/constants/iconsMap';
 
 export interface PlantCardProps {
   readonly plant: Plant;
@@ -19,8 +21,20 @@ export default function PlantCard({ plant }: PlantCardProps) {
 
         {plant.identity.scientificName && (
           <p className="plant-scientific-name">
-            {capitalizeFirstLetter(plant.identity.scientificName)}
+            <Badge variant="scientific" icon={iconsMap.research}>
+              {capitalizeFirstLetter(plant.identity.scientificName)}
+            </Badge>
           </p>
+        )}
+
+        {plant.identity.name.aliases.length > 0 && (
+          <div className="plant-aliases">
+            {plant.identity.name.aliases.map((alias) => (
+              <Badge key={alias} variant="alias" icon={iconsMap.tag}>
+                {capitalizeFirstLetter(alias)}
+              </Badge>
+            ))}
+          </div>
         )}
       </article>
     </Link>
